60a1e1ca6def8ea5b5c8046e6425eeb6a72c7179
Samuel Rezende <sam_profissional@outlook.com>
2025-10-25 14:04:54 -0300

    feat: add ConfigModule and MongooseModule to AppModule, update .env.dev in .gitignore, and modify start:dev script in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { InvoicesModule } from './invoices/invoices.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
-import { InvoicesModule } from './invoices/invoices.module';
 
 @Module({
-  imports: [OrdersModule, ProductsModule, InvoicesModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.LOCAL_DB_URI!),
+    InvoicesModule,
+    OrdersModule,
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
