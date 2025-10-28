212d37cd3acedcba75a036455086ab189cc0bfbf
Samuel Rezende <sam_profissional@outlook.com>
2025-10-27 21:55:15 -0300

    feat: add fundamental properties to Product schema including id, serialNumber, EANCode, category, subcategory, brand, model, description, dimensions, and stock

--- src/products/schemas/product.schema.ts
+++ src/products/schemas/product.schema.ts
@@ -6,7 +6,52 @@ export type ProductDocument = HydratedDocument<Product>;
 @Schema({ timestamps: true })
 export class Product {
   @Prop({ required: true })
+  id: number; // change to UUID later!!!
+
+  @Prop()
+  serialNumber: string; // also used as SKU
+
+  @Prop({ required: true })
+  EANCode: string;
+
+  @Prop({ required: true })
   name: string;
+
+  @Prop()
+  category: Array<string>; // change to enum later!!!
+
+  @Prop()
+  subcategory: string;
+
+  @Prop({ required: true })
+  brand: string;
+
+  @Prop({ required: true })
+  model: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  dimensions: {
+    height: number;
+    width: number;
+    depth: number;
+    weight: number;
+    volume: number;
+  };
+
+  @Prop({ required: true })
+  stock: {
+    location: string;
+    productStatus: string; // change to enum later!!!
+    expiryDate: Date;
+    manufactureDate: Date;
+    available: number;
+    reserved: number;
+    min: number;
+    max: number;
+  };
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
