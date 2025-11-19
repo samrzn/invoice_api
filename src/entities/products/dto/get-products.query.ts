eb77da7a27adb000e82f0f48b64fb58f311125da
Samuel Rezende <sam_profissional@outlook.com>
2025-11-18 21:07:34 -0300

    chore: improved validation pipes feature for update requests

--- src/entities/products/dto/get-products.query.ts
+++ src/entities/products/dto/get-products.query.ts
@@ -4,7 +4,6 @@ export class GetProductsQuery {
   @ApiPropertyOptional({
     type: String,
     description: 'Search keyword "name" to filter products',
-    example: 'Notebook',
   })
   keyword?: string;
 
